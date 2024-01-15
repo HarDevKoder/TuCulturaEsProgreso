38f3dd2856dbfc3c0f14eb4bc818d272dc8dc41c
HarDevKoder <haroldvaldes@yahoo.com>
2024-01-15 04:40:21 -0500

    Agrego codigo registro sw al main.js

--- js/main.js
+++ js/main.js
@@ -71,5 +71,9 @@ function overlayShow() {
     document.getElementById("overlay").style.display = "block";
   }
 }
-
-verificarServiceWorker();
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("../sw.js")
+      .then((reg) => console.log("Registro de SW exitoso", reg))
+      .catch((err) => console.warn("Error al tratar de registrar el sw", err));
+  }

956c6c95ecbd4bb0b7962488606ae156c5adcec9
HarDevKoder <haroldvaldes@yahoo.com>
2024-03-06 17:45:46 -0500

    cerrando cambios antiguos

--- vistas/loginVista.php
+++ vistas/loginVista.php
@@ -1,51 +1,52 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1,height=device-height, target-densitydpi=device-dpi">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600&display=swap" rel="stylesheet">   
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="../css/estilosLogin.css?v=16">
-    <link rel="shortcut icon" href="/imagenes/mejoradas/icon.png" type="image/x-icon">
- 
+  <meta charset="UTF-8">
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1, maximum-scale=1,height=device-height, target-densitydpi=device-dpi">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <title>Iniciar Sesión</title>
+  <link rel="stylesheet" href="../css/estilosLogin.css?v=16">
+  <link rel="shortcut icon" href="/imagenes/mejoradas/icon.png" type="image/x-icon">
+
 </head>
+
 <body>
 
-    <div class="contenedor">
-        <h1 class="titulo">Iniciar Sesión</h1>
-        <hr class="border">
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="formulario" name="login">
+  <div class="contenedor">
+    <h1 class="titulo">Iniciar Sesión</h1>
+    <hr class="border">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="formulario" name="login">
 
-            <div class="form-group">
-                <i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="Usuario">
-            </div>
-       
-            <div class="form-group">
-                <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="Contraseña">
-                <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
-            </div>
+      <div class="form-group">
+        <i class="icono izquierda fa fa-user"></i>
+        <input type="text" name="usuario" class="usuario" placeholder="Usuario">
+      </div>
 
-            <?php  if(!empty($errores)):  ?>
-                <div class="error">
-                    <ul>
-                        <?php echo $errores; ?>           
-                    </ul>       
-                </div>
-            <?php endif; ?>    
+      <div class="form-group">
+        <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn"
+          placeholder="Contraseña">
+        <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
+      </div>
 
-        </form>
+      <?php if (!empty($errores)) :  ?>
+      <div class="error">
+        <ul>
+          <?php echo $errores; ?>
+        </ul>
+      </div>
+      <?php endif; ?>
 
-        <!-- <p class="texto-registrate">
-            ¿Aún no tienes cuenta?
-            <a href="../php/registrate.php">Registrate</a>
-        </p> -->
+    </form>
 
-        <p class="texto-registrate">
-            <a href="../index.html">Salir</a>
-        </p>
-        
-     </div>   
+    <p class="texto-registrate">
+      <a href="../index.html">Salir</a>
+    </p>
+
+  </div>
 </body>
+
 </html>
